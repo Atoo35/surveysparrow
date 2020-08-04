5c4ae32eb83e5a251d4b0689640ca68c31091351
Atoo35 <43279227+Atoo35@users.noreply.github.com>
2020-08-04 16:19:10 +0530

    Update test.js

--- test.js
+++ test.js
@@ -6,9 +6,10 @@ app.use(express.json({extended: false}))
 
 //DBL webhooks
 app.post('/dblwebhook', async (req, res) => {
+  console.log(req)
   if(req.headers.authorization) {
     if(req.headers.authorization === config.webhook_secret) {
-      await thingToDo();
+      await thingToDo(req.vote);
       res.send({status: 200});
     }
     else {
@@ -23,15 +24,15 @@ app.post('/dblwebhook', async (req, res) => {
 // Launches the webserver on port 80
 function launchServer () {
   const http = require('http');
-  http.createServer(app).listen(80);
-  console.log(`Server started on port 80 pid: ${process.pid}`);
+  http.createServer(app).listen(process.env.PORT);
+  console.log(`Server started on port ${process.env.port} pid: ${process.pid}`);
 }
 
 launchServer();
 
 
-async function thingToDo () {
-  console.log('Webhook Successful!');
+async function thingToDo (vote) {
+  console.log('Webhook Successful!'+vote);
   /*
     This function is where you would put whatever "reward code" for the user upvoting.
     For example, our bot uses this function to add coins to the user in the database.
